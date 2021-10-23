fe36a57d6001338231b1beec3c862b163a98542f
mtake <mtake88@gmail.com>
2021-10-23 15:19:40 +0900

    初版

---
+++ sitka_highs.py
@@ -0,0 +1,40 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = "data/sitka_weather_2018_simple.csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    #ファイルから日付と最高気温を取得する
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+
+print(highs)
+print(len(highs))
+print(dates)
+print(len(dates))
+
+#最高気温のグラフを描画する
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c="red")
+
+#グラフにフォーマットを指定する
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel("",fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis="both", which="major", labelsize=16)
+
+plt.show()
