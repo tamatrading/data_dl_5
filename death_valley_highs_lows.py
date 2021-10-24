b3d183103eb1c32ee55d11522cfb41188ee3163b
mtake <mtake88@gmail.com>
2021-10-24 09:16:52 +0900

    修正1

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = "data/sitka_weather_2018_simple.csv"
+filename = "data/death_valley_2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -16,11 +16,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # print(highs)
 # print(len(highs))
@@ -35,7 +39,8 @@ ax.plot(dates, lows, c="blue", alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 #グラフにフォーマットを指定する
-plt.title("Daily high and low temperatures, 2018", fontsize=24)
+title = "Daily high and low temperatures - 2018 \nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel("",fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
